18869fd74598939b82f30b63727b7fa7b264e143
Christian Back <cdirt27@gmail.com>
2021-05-16 03:28:46 -0500

    setting up seperate router for grabbing details from movies table

---
+++ server/routes/detail.router.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool')
+
+router.get('/', (req, res) => {
+  // Add query to get all genres
+  const query = `SELECT * FROM genres ORDER BY name ASC`;
+  pool.query(query)
+    .then( result => {
+      console.log(result.data);
+      res.send(result.rows);
+    })
+    .catch(err => {
+      console.log('ERROR: Get all details', err);
+      res.sendStatus(500)
+    })
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(express.static('build'));
 /** ---------- ROUTES ---------- **/
 app.use('/api/movie', movieRouter);
 app.use('/api/genre', genreRouter)
-
+app.use('/api/detail', detailRouter)
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
     console.log('Listening on port: ', port);
