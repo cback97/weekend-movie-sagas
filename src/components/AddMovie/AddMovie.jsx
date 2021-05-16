6921bbfbc9a621eaed4f3c8f6a309f71b789e0d7
Christian Back <cdirt27@gmail.com>
2021-05-15 19:30:03 -0500

    edited styling on dropdown menu, nav routes for home and addmovie working, trying to get data from genres table to append to dropdown menu

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -1,6 +1,12 @@
 import './AddMovie.css';
+import { useSelector } from 'react-redux'
+
+
 
 function AddMovie() {
+
+    const genresReducer = useSelector(store => store.genres)
+    console.log(genresReducer);
 
     return (
         <>
@@ -20,20 +26,9 @@ function AddMovie() {
                 <div className="dropdown">
                     <button className="dropbtn">Hover to Select Movie Genre</button>
                     <div className="dropdown-content">
-                        <a href="#">Adventure </a>
-                        <a href="#">Animated </a>
-                        <a href="#">Biographical </a>
-                        <a href="#">Comedy </a>
-                        <a href="#">Disaster </a>
-                        <a href="#">Drama </a>
-                        <a href="#">Epic </a>
-                        <a href="#">Fantasy </a>
-                        <a href="#">MusicalMusical </a>
-                        <a href="#">Romantic </a>
-                        <a href="#">Science Fiction </a>
-                        <a href="#">Space-Opera </a>
-                        <a href="#">Superhero </a>
-                        <option value=""></option>
+                        {genresReducer.map((item, i) =>
+                            <option key={i}> {item.name} </option>
+                        )}
                     </div>
                 </div>
             </form>
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import {HashRouter as Router, Route, Link} from 'react-router-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList';
 import AddMovie from '../AddMovie/AddMovie';
@@ -8,27 +8,27 @@ function App() {
   return (
     <div className="App">
       <h1>The Movies Saga!</h1>
-      <Router>    
-      <nav>
-                    <ul>
-                        <li>
-                            <Link to = "/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to = "/AddMovie">AddMovie</Link>
-                        </li>
-                    </ul>
-                </nav>    
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/AddMovie">AddMovie</Link>
+            </li>
+          </ul>
+        </nav>
         <Route path="/" exact>
           <MovieList />
         </Route>
-        
+
         {/* Details page */}
 
         <Route path="/AddMovie">
           <AddMovie />
         </Route>
-      
+
       </Router>
     </div>
   );
